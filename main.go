660ad06687c0170565aa710573071c2b7e703cdf
Leo Hirano <66753378+notLeoHirano@users.noreply.github.com>
2025-11-02 16:09:24 -0500

    init commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+
+	
+}
---
+++ main_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+	"time"
+)
